2789ec6af7b17efbb9b6bf5b05d22f456b643b50
prakashsingh471 <prakashkumarsingh471@gmail.com>
2025-03-23 23:20:03 +0530

    first commit

---
+++ db_connect.js
@@ -0,0 +1,31 @@
+// const mongoose = require("mongoose")
+
+// async function getConnect(){
+//     try {
+//         await mongoose.connect(process.env.DB_KEY)
+//         console.log("Data Base is Connected")
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// getConnect()
+
+
+// const mongoose = require("mongoose")
+// mongoose.connect(process.env.DB_KEY)
+//     .then(()=>{
+//         console.log("Data Base is Connected")
+//     })
+//     .catch(error =>{
+//         console.log(error)
+//     })  
+
+require("mongoose")
+       .connect(process.env.DB_KEY)
+       .then(()=>{
+        console.log("Data Base is connected")
+       })
+       .catch(error=>{
+        console.log(error)
+       })
+
